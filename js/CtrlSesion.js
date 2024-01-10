8368099be7da90c37c66095c9bac0fd9e3e1d495
forms5nv50 <152238731+forms5nv50@users.noreply.github.com>
2024-01-10 09:14:12 -0600

    Update CtrlSesion.js

--- js/CtrlSesion.js
+++ js/CtrlSesion.js
@@ -54,12 +54,12 @@ async function muestraSesión(usuario) {
   }
 }
 
-function updateUser(user, usuario){
+function updateUser(usuario){
   var userNameH1 = document.getElementById("user_name");
   if (usuario.email == "forms5nv50@gmail.com") {
     userNameH1.innerHTML = "Hola Angel Hernandez Martinez! Qué deseas comprar hoy?";
   } else {
-  userNameH1.innerHTML = "Hola " + user.displayName + "! Qué deseas comprar hoy?";
+  userNameH1.innerHTML = "Hola " + usuario.displayName + "! Qué deseas comprar hoy?";
   }
 }
 
