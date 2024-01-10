a89045c97f08abe6859636cd84af1f27bd3e3ffd
forms5nv50 <152238731+forms5nv50@users.noreply.github.com>
2024-01-10 06:43:05 -0600

    Update CtrlSesion.js

--- js/CtrlSesion.js
+++ js/CtrlSesion.js
@@ -1,23 +1,22 @@
 import {
   getAuth
-} from "../lib/fabrica.js";
+} from ";../lib/fabrica.js";;
 import {
   muestraError
-} from "../lib/util.js";
+} from ";../lib/util.js";;
 import {
   iniciaSesión,
   terminaSesión
-} from "./seguridad.js";
+} from ";./seguridad.js";;
 import { 
   asignarRolCliente,
   asignarRolAdmin
-} from "./asignarRolCliente.js";
+} from ";./asignarRolCliente.js";;
 
 /** @type {HTMLFormElement} */
-const forma = document["forma"];
+const forma = document[";forma";];
 /** @type {HTMLImageElement} */
-const avatar = document.
-  querySelector("#avatar");
+const avatar = document.querySelector(";#avatar";);
 
 /* Escucha cambios de usuario.
  * El primer parámetro es una
@@ -32,57 +31,53 @@ const avatar = document.
 getAuth().onAuthStateChanged(
   muestraSesión, muestraError);
 
-
 /** Muestra los datos del usuario
  * o manda a iniciar sesión en
  * caso de que no haya empezado.
  * @param {import(
-    "../lib/tiposFire").
+    ";../lib/tiposFire";).
     User} usuario modelo con las
  *    características del usuario
  *    o null si no ha iniciado
  *    sesión. */
-function updateUser(user){
-      var userNameH1 = document.getElementById('user_name');
-      userNameH1.innerHTML = 'Hola, ' + user.displayName;
-    }
-
-function loginWithGoogle() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    return firebase.auth().signInWithPopup(provider);
-}
-
-async function
-  muestraSesión(usuario) {
-  if (usuario && usuario.email) {
-    if (usuario.email == 'forms5nv50@gmail.com') {
+async function muestraSesión(usuario) {
+  if (usuario &amp;&amp; usuario.email) {
+    if (usuario.email == "forms5nv50@gmail.com") {
       // Usuario aceptado.
       const userId = usuario.email;
-      // Asigna el rol de "Admin" al usuario
+      // Asigna el rol de ";Admin"; al usuario
       asignarRolAdmin(userId);
     } else {
       // Obtén el ID o correo electrónico del usuario al que deseas asignar el rol
       const userId = usuario.email;
-      // Asigna el rol de "Cliente" al usuario
+      // Asigna el rol de ";Cliente"; al usuario
       asignarRolCliente(userId);
     }
-    loginWithGoogle().then(function(result) {
-        const user = result.user;
-        updateUser(user);
-    }).catch(function(error) {
-        console.error(error);
-    });
-    forma.email.value =
-      usuario.email || "";
-    loginWithGoogle();
-    avatar.src =
-      usuario.photoURL || "";
-    forma.terminarSesión.
-      addEventListener(
-        "click", terminaSesión);
+    forma.email.value = usuario.email || ";";;
+    avatar.src = usuario.photoURL || ";";;
+    forma.terminarSesión.addEventListener(";click";, terminaSesión);
   } else {
     // No ha iniciado sesión.
     iniciaSesión();
   }
 }
 
+function updateUser(user){
+  var userNameH1 = document.getElementById("user_name");
+  userNameH1.innerHTML = "Hola, " + user.displayName;
+}
+
+function loginWithGoogle() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  return firebase.auth().signInWithRedirect(provider);
+}
+
+// Maneja el resultado del inicio de sesión
+firebase.auth().getRedirectResult().then(function(result) {
+    if (result.user) {
+        // Actualiza los detalles del usuario aquí
+        updateUser(result.user);
+    }
+  }).catch(function(error) {
+    console.error(error);
+  });
