b76b7e3919461323b0ab5f22edc6960472b28ba8
forms5nv50 <152238731+forms5nv50@users.noreply.github.com>
2024-01-10 11:00:35 -0600

    Update CtrlAlumnoNuevo.js

--- js/CtrlAlumnoNuevo.js
+++ js/CtrlAlumnoNuevo.js
@@ -13,9 +13,7 @@ import {
   tieneRol
 } from "./seguridad.js";
 
-const daoAlumno =
-  getFirestore().
-    collection("Alumno");
+const db = getFirestore();
 /** @type {HTMLFormElement} */
 const forma = document["forma"];
 getAuth().onAuthStateChanged(
@@ -28,38 +26,24 @@ async function protege(usuario) {
   if (tieneRol(usuario,
     ["Administrador"])) {
     forma.addEventListener(
-      "submit", guarda);
+      "submit", guardar);
   }
 }
 
 /** @param {Event} evt */
-async function guarda(evt) {
-  try {
-    evt.preventDefault();
-    const formData =
-      new FormData(forma);
-    const matricula = getString(
-        formData, "matricula").trim();  
-    const nombre = getString(formData, "nombre").trim();
-    const telefono = getString(formData, "telefono").trim();
-    const grupo = getString(formData, "grupo").trim();
-    const fecha = getString(formData, "fecha").trim();
-    /**
-     * @type {
-        import("./tipos.js").
-                Alumno} */
-    const modelo = {
-      matricula,
-      nombre,
-      telefono,
-      grupo,
-      fecha 
-    };
-    await daoAlumno.
-      add(modelo);
-    muestraAlumnos();
-  } catch (e) {
-    muestraError(e);
-  }
+async function guardar(){
+  var nombre = document.getElementById('nombre').value;
+  var descripcion = document.getElementById('descripcion').value;
+  var precio = document.getElementById('precio').value;
+  var color = document.getElementById('color').value;
+  var fechaLanzamiento = document.getElementById('fechaLanzamiento').value;
+  let fecha = new Date(fechaLanzamiento);
+  let fechaFirebase = firebase.firestore.Timestamp.fromDate(fecha);
+  db.collection("HerramientasApple").add({
+    nombre: nombre,
+    descrpcicion: descripcion,
+    precio: precio,
+    color: color,
+    fechaLanzamiento: fechaFirebase
+  })
 }
-
